8a144697a94b40d3c2c011b6d819f49ea336ad83
Enrique Fermin <efermin@aaxisdigital.com>
2023-04-26 18:17:18 -0400

    Navbar modified

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -4,7 +4,7 @@ export const GET_RECIPES_BY_NAME = "GET_RECIPES_BY_NAME";
 
 export function getAllRecipes() {
 
-  let url = `http://localhost:3001/recipes/`;
+  let url = `${API_URL}/recipes/`;
   const response = axios.get(url);
 
   return  response;
@@ -12,7 +12,7 @@ export function getAllRecipes() {
 
 export function getRecipesByName(recipeName) {
 
-  let url = `http://localhost:3001/recipes/name?name=${recipeName}`;
+  let url = `${API_URL}/recipes/name?name=${recipeName}`;
   const response = axios.get(url);
   return  response;
 }
@@ -20,7 +20,7 @@ export function getRecipesByName(recipeName) {
 
 export const createRecipe = async (recipeData) => {
   try {
-    const response = await axios.post('http://localhost:3001/recipes', recipeData);
+    const response = await axios.post(`${API_URL}/recipes`, recipeData);
     return response.data;
   } catch (error) {
     throw new Error(error.message);
@@ -28,7 +28,7 @@ export const createRecipe = async (recipeData) => {
 };
 
 export const filterRecipesByDiet = (filterOption) => {
-  let url = `http://localhost:3001/recipes/diet?diet=${filterOption}`;
+  let url = `${API_URL}/recipes/diet?diet=${filterOption}`;
   const response = axios.get(url);
   return  response;
 };
