bf42ce304303ad70386d6012ab342b0fb4a545a6
Adri-ESM <anggivmorales@gmail.com>
2023-04-28 13:35:56 -0400

    responsive filter

--- client/src/components/filter/Filter.jsx
+++ client/src/components/filter/Filter.jsx
@@ -37,9 +37,9 @@ export default function Filter({ onFilter, onHealthScore }) {
     <div className={styles.filterDietsHealth}>
       <div className={styles.filterContainer}>
         <label>
-          <span>Filter by Diets:</span>
+          <span>Types of Diets:</span>
           <select value={selectedOption} onChange={handleOptionChange}>
-            <option value=""></option>
+            <option className={styles.option} value="">Search</option>
             {data.map((option) => (
               <option value={option.name} key={option.id}>{option.name}</option>
             ))}
@@ -48,17 +48,20 @@ export default function Filter({ onFilter, onHealthScore }) {
         <button className={styles.filterButton} onClick={handleFilterClick}>Filter</button>
       </div>
 
+    <div className={styles.filterHealthContainer}>
       <form onSubmit={handleSubmit}>
+      <span>By Health:</span>
         <input
           name='filterHealthScore'
           value={filterHealthScore}
           onChange={handleInputChange}
-          placeholder='Search By Health'
+          placeholder='Search'
         />
-        <div className={styles.searchButton}>
-          <button type='submit'>Search</button>
-        </div>
       </form>
+      <div>
+          <button className={styles.filterButton} type='submit'>Filter</button>
+        </div>
+    </div>
     </div>
   );
 }
